d41a1e447b8afcf2d3e789f2db1a9fc02a3f1f87
satyadev20 <garikapatisrisatyadev@gmail.com>
2023-06-28 15:43:53 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,9 +9,14 @@ app = FastAPI()
 def read_root():
     return {"Hello": "SatyaDev"}
 
+@app.get("/sri")
+def read_root():
+    return {"Hello": "Sri"}
+    
 @app.get("/satya")
 def read_root():
     return {"Hello": "Satya"}
+    
 @app.get("/dev")
 def read_root():
     return {"Hello": "Dev"}
